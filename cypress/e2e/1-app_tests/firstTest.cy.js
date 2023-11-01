29c47d7405ce99746feeba57839e904a736fff2b
MaryIvanisovaa <ivanisovamaria07@gmail.com>
2023-11-01 17:29:01 +0200

    new commit with prettier

--- cypress/e2e/1-app_tests/firstTest.cy.js
+++ cypress/e2e/1-app_tests/firstTest.cy.js
@@ -1,20 +1,17 @@
 /// <reference types="cypress" />
 
-describe('First test suite', ()=>{
-    context('First test suite-context', ()=>{
-
-
-it ('Verify url', ()=>{
-cy.visit('layout/stepper')
-cy.url().should('contain', 'layout/stepper');
-});
-});
+describe("First test suite", () => {
+  context("First test suite-context", () => {
+    it("Verify url", () => {
+      cy.visit("layout/stepper");
+      cy.url().should("contain", "layout/stepper");
+    });
+  });
 });
 
-
-describe('Second describe inside', ()=>{
-it('Check text of button "Hide on click"', ()=>{
-cy.visit ('modal-overlays/toastr')
-cy.get('span.text').should('contain', 'Hide on click');
-});
+describe("Second describe inside", () => {
+  it('Check text of button "Hide on click"', () => {
+    cy.visit("modal-overlays/toastr");
+    cy.get("span.text").should("contain", "Hide on click");
+  });
 });
